02407b3fdb482cc9a2d4973cd35f13cf89783d7a
Spartfin <fin.07@bk.ru>
2019-12-13 17:34:19 +0300

    Добавлена реализация js блока Тренеры

--- source/js/main.js
+++ source/js/main.js
@@ -49,19 +49,19 @@
   // Проверяем фокус
   inp.addEventListener('focus', _ => {
     // Если там ничего нет или есть, но левое
-    if(!/^\+\d*$/.test(inp.value))
+    if (!/^\+\d*$/.test(inp.value))
       // То вставляем знак плюса как значение
       inp.value = '+7';
   });
 
   inp.addEventListener('keypress', e => {
     // Отменяем ввод не цифр
-    if(!/\d/.test(e.key))
+    if (!/\d/.test(e.key))
       e.preventDefault();
   });
 
 
-  // Слайдер блоке отзывы
+  // Слайдер блока отзывы
   var next = document.querySelector('.reviews__click_right');
   var prew = document.querySelector('.reviews__click_left');
 
@@ -99,5 +99,36 @@
       }
     }
   });
+
+  // Слайдер блока тренеры
+  var activeTrainer = 0;
+  var prevTrainer = 0;
+  var leftArrow = document.querySelector('.trainers__click_left');
+  var rightArrow = document.querySelector('.trainers__click_right');
+  var slidesTrainers = document.getElementsByClassName('trainers__list_card');
+
+  var clickRightArrow = function () {
+    rightArrow.addEventListener('click', function () {
+      activeTrainer >= 0 && activeTrainer < slidesTrainers.length - 1 ? ++activeTrainer : activeTrainer = 0;
+      changeActiveSlide();
+    });
+  };
+  clickRightArrow();
+
+  var clickLeftArrow = function () {
+    leftArrow.addEventListener('click', function () {
+      activeTrainer > 0 ? --activeTrainer : activeTrainer = slidesTrainers.length - 1;
+      changeActiveSlide();
+    });
+  };
+  clickLeftArrow();
+
+  var changeActiveSlide = function () {
+    slidesTrainers[prevTrainer].classList.remove('slide__Active');
+    slidesTrainers[activeTrainer].classList.add('slide__Active');
+    for (var i = 0; i < slidesTrainers.length; i++) {
+      slidesTrainers[i].style.transform = 'translate(' + -(activeTrainer * 226) + 'px)';
+    }
+  };
 
 })();
